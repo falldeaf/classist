c4af82bef883b25f98c90da8a35e2de73d86ed03
Tom Mardis <falldeaf@gmail.com>
2022-05-31 14:38:40 -0400

    batch file updates

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ CALL androidjs build
 CALL C:\Users\tmardis\AppData\Local\Android\Sdk\platform-tools\adb -s %1 install .\dist\classist.apk
 Start-Sleep -Seconds 1
 CALL C:\Users\tmardis\AppData\Local\Android\Sdk\platform-tools\adb -s %1 shell monkey -p com.androidjs.classist 1
-powershell -c (New-Object Media.SoundPlayer "C:\Users\tmardis\Downloads\notify.wav").PlaySync();
+powershell "[console]::beep(1000,500)"
